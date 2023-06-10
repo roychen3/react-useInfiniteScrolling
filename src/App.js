ce63b7e900bcf66ac7e7b94deda6a0132011a9f8
Roy Chen <roy850323@gmail.com>
2023-06-10 18:05:04 +0800

    format doc

--- src/App.js
+++ src/App.js
@@ -5,44 +5,47 @@ import { useInfiniteScrolling } from './hook/useInfiniteScrolling';
 import './App.css';
 
 function App() {
-  const [todoList, setTodoList] = useState([])
-  const [dataLoading, setDataLoading] = useState(false)
-  const [startId, setStartId] = useState(0)
-  const [hasMore, setHasMore] = useState(false)
-
+  const [todoList, setTodoList] = useState([]);
+  const [dataLoading, setDataLoading] = useState(false);
+  const [startId, setStartId] = useState(0);
+  const [hasMore, setHasMore] = useState(false);
 
   const fetchData = useCallback(async () => {
-    setDataLoading(true)
-    const response = await fetch(`https://jsonplaceholder.typicode.com/todos?_start=${startId}&_limit=40`)
+    setDataLoading(true);
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/todos?_start=${startId}&_limit=40`
+    );
     const responseData = await response.json();
-    const responseDataLength = responseData.length
+    const responseDataLength = responseData.length;
     if (responseDataLength > 0) {
-      setTodoList((preTodoList) => [...preTodoList, ...responseData])
-      setStartId(responseData[responseDataLength - 1].id)
-      setHasMore(true)
+      setTodoList((preTodoList) => [...preTodoList, ...responseData]);
+      setStartId(responseData[responseDataLength - 1].id);
+      setHasMore(true);
     } else {
-      setHasMore(false)
+      setHasMore(false);
     }
     setDataLoading(false);
   }, [startId]);
 
   const handleInfiniteScrollingTrigger = useCallback(() => {
     if (hasMore && dataLoading === false) {
-      fetchData()
-        .catch((error) => { console.log(error.message) });
-    };
+      fetchData().catch((error) => {
+        console.log(error.message);
+      });
+    }
   }, [hasMore, dataLoading, fetchData]);
   const lastElementRef = useInfiniteScrolling(handleInfiniteScrollingTrigger);
 
   useEffect(() => {
     if (todoList.length === 0) {
-      fetchData()
-        .catch((error) => { console.log(error.message) });
+      fetchData().catch((error) => {
+        console.log(error.message);
+      });
     }
-  }, [todoList, fetchData])
+  }, [todoList, fetchData]);
 
   return (
-    <div className='list'>
+    <div className="list">
       {todoList.map((item, itemIdx) => (
         <>
           {itemIdx + 1 === todoList.length ? (
